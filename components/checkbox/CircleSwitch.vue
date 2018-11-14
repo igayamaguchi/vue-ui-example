ad5dca02ff36913badb59bc6109364cfc6fcd145
iga-yamaguchi <iganin2@gmail.com>
2018-11-15 01:49:33 +0900

    WIP: circle switcher

---
+++ components/checkbox/CircleSwitch.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <label @click.prevent="onCheck">
+      <span v-if="description">{{ description }}</span>
+      <input
+        :checked="checked"
+        type="checkbox">
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CircleSwitch',
+  props: {
+    checked: {
+      required: true,
+      type: Boolean
+    },
+    description: {
+      required: false,
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    onCheck() {
+      this.$emit('check', !this.checked)
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  position: relative;
+}
+input {
+  opacity: 0;
+}
+label::before {
+  content: '';
+  background-color: black;
+  width: 1rem;
+  height: 1rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
---
+++ pages/checkbox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Checkbox</h1>
+    <circle-switch
+      :checked="onOff"
+      description="CircleSwitch"
+      @check="circleSwitch"/>
+  </div>
+</template>
+
+<script>
+import CircleSwitch from '~/components/checkbox/CircleSwitch.vue'
+
+export default {
+  name: 'Checkbox',
+  components: { CircleSwitch },
+  data() {
+    return {
+      onOff: false
+    }
+  },
+  methods: {
+    circleSwitch(value) {
+      this.onOff = value
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
