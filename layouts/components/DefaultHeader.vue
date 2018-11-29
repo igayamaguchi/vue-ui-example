01b5ede68391c21fa6e9c2f8fba60ae250365a94
iga-yamaguchi <iganin2@gmail.com>
2018-11-30 00:18:47 +0900

    WIP: search

--- layouts/components/DefaultHeader.vue
+++ layouts/components/DefaultHeader.vue
@@ -5,6 +5,7 @@
       <router-link to="button">Button</router-link>
       <router-link to="card">Card</router-link>
       <router-link to="carousel">Carousel</router-link>
+      <router-link to="search">Search</router-link>
     </div>
   </header>
 </template>
---
+++ pages/search.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <h1>Search</h1>
+    <form action="">
+      <dl>
+        <dt>キーワード</dt>
+        <dd>
+          <label>
+            <input
+              type="text"
+              name="keyword">
+          </label>
+        </dd>
+        <dt>チェックイン</dt>
+        <dd>
+          <label>
+            <input
+              type="text"
+              name="checkin">
+          </label>
+        </dd>
+        <dt>チェックアウト</dt>
+        <dd><label>
+          <input
+            type="text"
+            name="checkout">
+        </label></dd>
+        <dt>大人</dt>
+        <dd>
+          <label>
+            <input
+              type="text"
+              name="adult">
+          </label>
+        </dd>
+        <dt>子供</dt>
+        <dd>
+          <label>
+            <input
+              type="text"
+              name="child">
+          </label>
+        </dd>
+        <dt>最低価格</dt>
+        <dd>
+          <label>
+            <input
+              type="text"
+              name="minimum">
+          </label>
+        </dd>
+      </dl>
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+      keyword: '',
+      checkin: '',
+      checkout: '',
+      adult: '',
+      child: '',
+      minimum: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
