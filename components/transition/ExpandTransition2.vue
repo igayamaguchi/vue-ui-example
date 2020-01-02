c3de8024b3d1601e087851e27ba8972f28ed9433
igayamaguchi <iganin2@gmail.com>
2020-01-03 00:26:00 +0900

    add right expand transition

---
+++ components/transition/ExpandTransition2.vue
@@ -0,0 +1,22 @@
+<template>
+  <transition name="expand">
+    <slot />
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'ExpandTransition2'
+}
+</script>
+
+<style scoped>
+.expand-enter-active,
+.expand-leave-active {
+  transition: height 0.5s;
+}
+.expand-enter,
+.expand-leave-to {
+  height: 0;
+}
+</style>
--- pages/transition.vue
+++ pages/transition.vue
@@ -4,17 +4,21 @@
     <div>
       <button @click="expand = !expand">Expand</button>
       <expand-transition>
-        <div v-show="expand" :class="$style.expandBox"></div>
+        <div v-show="false" :class="$style.expandBox"></div>
       </expand-transition>
+      <expand-transition2>
+        <div v-show="expand" :class="$style.expandBox"></div>
+      </expand-transition2>
     </div>
   </div>
 </template>
 
 <script>
 import ExpandTransition from '@/components/transition/ExpandTransition.vue'
+import ExpandTransition2 from '@/components/transition/ExpandTransition2.vue'
 export default {
   name: 'Transition',
-  components: { ExpandTransition },
+  components: { ExpandTransition, ExpandTransition2 },
   data() {
     return { expand: false }
   }
